9cf8466bde439413ebc3385c3c3928f1c3fee4c2
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-05-28 13:19:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+/*
+
+const asyncFn = async () =>{
+    return ("Success!");
+} 
+
+asyncFn();
+
+*/
+
+/*
+
+const asyncFn = async () =>{
+    return 10;
+} 
+
+asyncFn();
+
+*/
+
+/*
+
+const asyncFn = async () =>{
+    return 10;
+} 
+
+asyncFn()
+.then(value => console.log(value))
+
+*/
+
+/*
+
+const asyncFn = async () => {
+    throw new Error ("Big Error")
+    
+}
+asyncFn();
+
+*/
+
